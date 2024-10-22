055f9b36e799c88b16b2e09c96a4c6e7a9299f25
dharryc <dharrisoncook@gmail.com>
2024-10-21 21:54:05 -0600

    finished

--- site.js
+++ site.js
@@ -3,16 +3,25 @@ import { products } from "./products.js"
 //setup card making function
 const totalPriceNode = document.getElementById("amount")
 totalPriceNode.textContent = "0.00"
-const productsNode = document.getElementById("products-container")
-productsNode.addEventListener("drop", (ev) => {
+const cardArticleElement = document.getElementById("products-container")
+cardArticleElement.addEventListener("drop", (ev) => {
     ev.preventDefault()
     const cartListNode = document.getElementById(ev.dataTransfer.getData("id")).parentElement
     const currentProductNode = document.getElementById(ev.dataTransfer.getData("id"))
-    console.log(ev.dataTransfer.getData("id"))
+    let nonCopyNode = document.getElementById(`${currentProductNode.childNodes[1].childNodes[0].textContent}`)
+    if(nonCopyNode === null){
+        nonCopyNode = MakeCards(products.find((product) => product.title === `${currentProductNode.childNodes[1].childNodes[0].textContent}`))
+        nonCopyNode.childNodes[1].childNodes[3].textContent = 0
+        cardArticleElement.appendChild(nonCopyNode)
+    }
+    currentProductNode.childNodes[1].childNodes[3].textContent --
     if (cartListNode.id === "cartList") {
-        totalPriceNode.textContent = Number(totalPriceNode.textContent) - Number(currentProductNode.childNodes[1].childNodes[2].textContent)
+        totalPriceNode.textContent = (Number(totalPriceNode.textContent) - Number(currentProductNode.childNodes[1].childNodes[2].textContent)).toFixed(2)
+    }
+    if (Number(currentProductNode.childNodes[1].childNodes[3].textContent) === 0) {
         cartListNode.removeChild(currentProductNode)
     }
+    nonCopyNode.childNodes[1].childNodes[3].textContent ++
 })
 const cartNode = document.getElementById("cartList")
 cartNode.addEventListener("dragover", (ev) => {
@@ -20,20 +29,28 @@ cartNode.addEventListener("dragover", (ev) => {
 })
 cartNode.addEventListener("drop", (ev) => {
     ev.preventDefault()
-    const itemNode = document.getElementById(ev.dataTransfer.getData("id"))
-    totalPriceNode.textContent = Number(totalPriceNode.textContent) + Number(itemNode.childNodes[1].childNodes[2].textContent)
-    const myProduct = productList.find((product) => product.title === ev.dataTransfer.getData("id"))
-    const itemCopy = MakeCards(myProduct)
-    itemCopy.id = `${myProduct.title}-copy`
-    if (document.getElementById(`${myProduct.title}-copy`) == null) {
-        cartNode.appendChild(itemCopy)
+    if (!ev.dataTransfer.getData("id").includes("-copy")) {
+
+        const itemNode = document.getElementById(ev.dataTransfer.getData("id"))
+        totalPriceNode.textContent = (Number(totalPriceNode.textContent) + Number(itemNode.childNodes[1].childNodes[2].textContent)).toFixed(2)
+        const myProduct = productList.find((product) => product.title === ev.dataTransfer.getData("id"))
+        const itemCopy = MakeCards(myProduct)
+        itemCopy.id = `${myProduct.title}-copy`
+        itemCopy.childNodes[1].childNodes[3].textContent = 1
+        if (document.getElementById(`${myProduct.title}-copy`) == null) {
+            cartNode.appendChild(itemCopy)
+        }
+        else {
+            const itemQuantity = document.getElementById(`${myProduct.title}-copy`).childNodes[1].childNodes[3]
+            itemQuantity.textContent = Number(itemQuantity.textContent) + Number(1)
+        }
+        itemNode.childNodes[1].childNodes[3].textContent = Number(itemNode.childNodes[1].childNodes[3].textContent) - 1
+        if (Number(itemNode.childNodes[1].childNodes[3].textContent) === 0) {
+            itemNode.parentNode.removeChild(itemNode)
+        }
     }
-    itemNode.childNodes[1].childNodes[3].textContent = Number(itemNode.childNodes[1].childNodes[3].textContent) - 1
-    if (Number(itemNode.childNodes[1].childNodes[3].textContent) === 0)
-        itemNode.parentNode.removeChild(itemNode)
 })
 
-const cardArticleElement = document.getElementById("products-container")
 const productList = products.map(a => a);
 const availibleProducts = document.createElement("h2")
 availibleProducts.textContent = "Availible Products"
